faf81550e7d0809c98052e4bd43d649bb07417f5
eric <ericlittlefield@comcast.net>
2022-01-06 08:18:29 -0700

    extended solution to support quickbot_rpi

--- run.py
+++ run.py
@@ -12,9 +12,19 @@
 import sys
 import argparse
 
+def get_ip(iface = 'eth0'):
+    ifreq = struct.pack('16sH14s', iface, socket.AF_INET, '\x00'*14)
+    try:
+        res = fcntl.ioctl(sockfd, SIOCGIFADDR, ifreq)
+    except:
+        return None
+    ip = struct.unpack('16sH2x4s8x', res)[2]
+    return socket.inet_ntoa(ip)
+
+
 
 DESCRIPTION = ""
-RTYPES = ('quick_v2', 'quick_v1', 'ultra')
+RTYPES = ('quick_v2', 'quick_v1', 'ultra', 'quick_rpi')
 
 
 def main(options):
@@ -29,15 +39,18 @@ def main(options):
     if options.rtype == 'quick_v2':
         import xbots.quickbot_v2
         qb = xbots.quickbot_v2.QuickBot(options.ip, options.rip)
-        qb.run()
     elif options.rtype == 'quick_v1':
         import xbots.quickbot_v1
         qb = xbots.quickbot_v1.QuickBot(options.ip, options.rip)
-        qb.run()
     elif options.rtype == 'ultra':
         import xbots.ultrabot
         qb = xbots.ultrabot.UltraBot(options.ip, options.rip)
-        qb.run()
+    elif option.rtype == 'quick_rpi':
+        qb = xbots.quickbot_rpi.QuickBot_rpi(options.ip, option.rip)
+    else:
+        raise Exception("No valid robot target provided")
+
+    qb.run()
 
 
 if __name__ == '__main__':
@@ -48,11 +61,11 @@ if __name__ == '__main__':
         help="Computer ip (base ip)")
     parser.add_argument(
         '--rip', '-r',
-        default='192.168.7.2',
-        help="BBB ip (robot ip)")
+        default=get_ip(),
+        help="robot ip")
     parser.add_argument(
         '--rtype', '-t',
-        default='quick_v2',
+        default='quick_rpi',
         help="Type of robot (%s)" % '|'.join(RTYPES))
 
     options = parser.parse_args()
